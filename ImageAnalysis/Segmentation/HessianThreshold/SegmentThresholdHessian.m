1138a7fbd5b628bc48a2b45230486bcef2c6dc44
mbarbie1 <mbarbie1@its.jnj.com>
2015-02-18 12:36:06 +0100

    Updated function descriptions

--- ImageAnalysis/Segmentation/HessianThreshold/SegmentThresholdHessian.m
+++ ImageAnalysis/Segmentation/HessianThreshold/SegmentThresholdHessian.m
@@ -1,9 +1,53 @@
-function lab = SegmentThresholdHessian(img, minRUnit, maxRUnit, minSizeUnit, maxSizeUnit, nScale, pixelSize, noiseThresh)
+% -----------------------------------------------------------------------
+% 
+% FUNCTION: segmentThresholdHessian
+% 
+% DESCRIPTION: Segmentation of objects in 3D image-stacks (grey-valued 
+%               images). 
+%               The (bright) objects have to be more or less
+%               compact in space for the algorithm and preferably 
+%               spheroidal.
+%
+%              Algorithm description:
+%               A labeled image is obtained by thresholding the hessian 
+%               diagonal elements of the image in scalespace. The diagonal
+%               elements of the Hessian approximate the curvature of the
+%               region of the pixel. When they're all negative it means 
+%               there is a region of maximum intensity --> a compact 
+%               bright object.
+% 
+% INPUT: 
+%           img                     : 3D image (DIPimage image)
+%           minRUnit                : minimal radius of objects in um (scalar)
+%           maxRUnit                : maximal radius of objects in um (scalar)
+%           minSizeUnit             : minimal size of the objects in um^3 (scalar)
+%           maxSizeUnit             : maximal size of the objects in um^3 (scalar)
+%           nScale                  : number of scales taken (exponantial
+%                                       spaced scales. (integer)
+%           pixelSize               : pixel size (x,y,z) (vector)
+%           noisThresh              : a small number which serves to remove
+%                                       pixels which have very low Hessian
+%                                       eigenvalues and could be resulting
+%                                       from the noise in the image. 
+%                                       (scalar)
+%
+% OUTPUT:
+%           lab                     : labeled image of the segmentation 
+%                                       (DIPimage image)
+%
+% AUTHOR: 
+% 
+% 	Michaël Barbier
+%   mbarbie1@its.jnj.com
+% 
+% ----------------------------------------------------------------------- 
+%
+function lab = segmentThresholdHessian(img, minRUnit, maxRUnit, minSizeUnit, maxSizeUnit, nScale, pixelSize, noiseThresh)
 
     scaleOverRadiusRatio = 1/2;
     minScale = minRUnit * scaleOverRadiusRatio;
     maxScale = maxRUnit * scaleOverRadiusRatio;
-    [s, ss1, ss2, ss3] = getScaleSpace(nScale,minScale,maxScale, pixelSize);
+    [~, ss1, ss2, ss3] = getScaleSpace(nScale,minScale,maxScale, pixelSize);
     [~,t] = threshold(img, 'otsu');
     imgt = threshold(bilateralf(img), 'fixed',0.1*t);
     imgt = bmajority(imgt);
--- ImageAnalysis/Segmentation/SegmentThresholdSimple.m
+++ ImageAnalysis/Segmentation/SegmentThresholdSimple.m
@@ -1,4 +1,28 @@
-function lab = SegmentThresholdSimple(img, minSizeUnit, maxSizeUnit, pixelSize)
+% -----------------------------------------------------------------------
+% 
+% FUNCTION: segmentThresholdSimple
+% 
+% DESCRIPTION: Segmentation of objects in a grey-valued 3D-image stacks by
+%                   a simple threshold method.
+% 
+% INPUT: 
+%           img                     : 2D/3D image (DIPimage image)
+%           minSizeUnit             : minimal size of the objects (scalar)
+%           maxSizeUnit             : maximal size of the objects (scalar)
+%           pixelSize               : pixel size (x,y,(z)) (vector)
+%
+% OUTPUT:
+%           lab                     : labeled image of the segmentation 
+%                                       (DIPimage image)
+%
+% AUTHOR: 
+% 
+% 	Michaël Barbier
+%   mbarbie1@its.jnj.com
+% 
+% ----------------------------------------------------------------------- 
+%
+function lab = segmentThresholdSimple(img, minSizeUnit, maxSizeUnit, pixelSize)
 
     [~,t] = threshold(img, 'otsu');
     imgt = threshold(bilateralf(img), 'fixed',1*t);
